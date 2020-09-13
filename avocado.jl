95570f2fda19c8769acb71d3f19710bd0eae82cd
garrettkinman <garrettkinman@gmail.com>
2020-09-13 01:06:45 -0400

    added some markdown

--- avocado.jl
+++ avocado.jl
@@ -11,7 +11,16 @@ md"## Data Analysis
 3.  Price vs Month
 4.  Price vs Type"
 
+# ╔═╡ f7215a00-f523-11ea-1674-cb60757a26e2
+md"Let's first take a look at one region in particular. We'll take the first region, Albany."
+
+# ╔═╡ 0a5d0ebe-f524-11ea-288d-1f7f8a6ea259
+
+
 # ╔═╡ c94fdee0-f513-11ea-38b6-e95f27de85bb
+md"We see that there are two clusters, with very different magnitudes of quantities, which makes it difficult to see what's going on in the left cluster. Let's split the Albany data into two, so we can see each cluster more clearly."
+
+# ╔═╡ 3bc239f0-f51e-11ea-1595-738f5b811660
 
 
 # ╔═╡ 54a16062-f512-11ea-17ae-f18d96e5ae5d
@@ -49,7 +58,10 @@ scatter(albany_prices[!, :"Total Volume"], albany_prices[!, :"AveragePrice"])
 # ╠═fb57c070-f512-11ea-338e-ef92ca1fa317
 # ╠═3e1811d0-f513-11ea-2b83-05cd42ff8bf9
 # ╠═894d26e0-f513-11ea-3ccb-db999354d3b2
+# ╠═f7215a00-f523-11ea-1674-cb60757a26e2
+# ╠═0a5d0ebe-f524-11ea-288d-1f7f8a6ea259
 # ╠═b6d975f0-f513-11ea-14a9-89bcff2918e1
 # ╠═9c37ae60-f513-11ea-3400-e9b0ce82d639
 # ╠═aeb56a50-f513-11ea-3f6c-1d49f3700c58
 # ╠═c94fdee0-f513-11ea-38b6-e95f27de85bb
+# ╠═3bc239f0-f51e-11ea-1595-738f5b811660
